21243ffad7b9e2abc4cd27bb1406ab9c665f6477
SpyrosD3v25 <code123go@gmail.com>
2022-02-20 07:57:43 +0200

    initial commit

---
+++ home/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Profile, BlogPost
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [] 
+        
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = ('title', 'content', 'image', 'category')
+        
+        category = forms.NullBooleanField()
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Title of the Blog'}), 
+            'content': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Content of the Blog'}),
+        }
